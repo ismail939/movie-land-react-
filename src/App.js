2b7617f52cce3039b413a0711c8f75fa729cd445
ismail939 <ismailsalama526@gmail.com>
2023-12-20 23:33:18 +0200

    react project started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Person = () =>{
+  return(
+    <>
+      <h1>Name: Ismail</h1>
+      <h2>Age: 23</h2>
+    </>
+  )
+}
+const App = () => {
+  const name = 'ismail'
+  let isNameShowing = false
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person />
     </div>
   );
 }
